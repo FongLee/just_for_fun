18db59f7f121afbd976186fdf0b4bda18edd639b
win10 <lifeng245112@126.com>
2018-05-31 22:39:06 +0800

    function

--- javascript/20180515-ES6.js
+++ javascript/20180515-ES6.js
@@ -1,17 +1,16 @@
-
 // ES5 规定，函数只能在顶层作用域和函数作用域之中声明，不能在块级作用域声明。
 // 在谷歌浏览器中运行并不会报错
 if (true) {
-	function test(argument) {
-		console.log("it is a test");
-	}
+    function test(argument) {
+        console.log("it is a test");
+    }
 }
 
 try {
-	function test(argument) {
-		console.log("it is a test");
-	}
-} catch(e) {
+    function test(argument) {
+        console.log("it is a test");
+    }
+} catch (e) {
 
 }
 
@@ -22,26 +21,26 @@ try {
 // 允许在块级作用域内声明函数。
 // 函数声明类似于var，即会提升到全局作用域或函数作用域的头部。
 // 同时，函数声明还会提升到所在的块级作用域的头部。
-function  f () {
-	console.log("i am outside ");
+function f() {
+    console.log("i am outside ");
 }
 
-(function(){
-	if(false) {
-		function f() {
-			console.log("i am inside");
-		}
-	}
+(function() {
+    if (false) {
+        function f() {
+            console.log("i am inside");
+        }
+    }
 
-	f();
+    f();
 
 }())
 
 // 暂时性死区
 if (true) {
-	// Uncaught ReferenceError: MAXIN is not defined
-	console.log(MAXIN);
-	const MAXIN="lifeng";
+    // Uncaught ReferenceError: MAXIN is not defined
+    console.log(MAXIN);
+    const MAXIN = "lifeng";
 }
 
 // Uncaught SyntaxError: Missing initializer in const declaration
@@ -53,46 +52,46 @@ window.b // undefined
 
 // 函数里面的this，如果函数不是作为对象的方法运行，而是单纯作为函数运行，this会指向顶层对象
 function f(argument) {
-	console.log(this);
+    console.log(this);
 }
 
 // 获取顶层对象
-function getGlobal () {
-	// 浏览器和web Worker(运行在浏览器后台的JavaScript)
-	if(self !== 'undefined') {
-		return self;
-	}
+function getGlobal() {
+    // 浏览器和web Worker(运行在浏览器后台的JavaScript)
+    if (self !== 'undefined') {
+        return self;
+    }
 
-	// 浏览器
-	if(window !== 'undefined') {
-		return window;
-	}
-	// Node环境
-	if(global !== 'undefined') {
-		return global;
-	}
+    // 浏览器
+    if (window !== 'undefined') {
+        return window;
+    }
+    // Node环境
+    if (global !== 'undefined') {
+        return global;
+    }
 }
 
 // 数组的解构赋值
 let [a, b, c] = [1, 2, 3];
 
 // x=null,默认值，=== undefined，才会使用默认值
-let [x=1] = [null];
+let [x = 1] = [null];
 
 
 // fooTest "aaa" 先找到同名属性，然后赋值给对应的变量，
-let {foo: fooTest} = {foo: 'aaa', bar: 'bbb'};
+let { foo: fooTest } = { foo: 'aaa', bar: 'bbb' };
 
 // x = 1
 // y = 5
-var {x, y=5} = {x: 1};
+var { x, y = 5 } = { x: 1 };
 
 // 报错
-let {foo: {bar}} = {baz: 'baz'};
+let { foo: { bar } } = { baz: 'baz' };
 
 
-let arr=[1, 2, 3];
-let {0: first, [arr.length - 1] : last} = arr;
+let arr = [1, 2, 3];
+let { 0: first, [arr.length - 1]: last } = arr;
 
 // 1
 first
@@ -103,73 +102,74 @@ last
 const [a, b, c, d, e] = 'hello';
 
 // 数值和布尔值的解构赋值
-let {toString: s} = 123;
+let { toString: s } = 123;
 
-let {toString: s} = true;
+let { toString: s } = true;
 
 // 函数的解构赋值
 function add([a, b]) {
-	return a + b;
+    return a + b;
 }
 
 add([1, 2]);
 
 // 函数的解构赋值 使用默认值
-function add({a=1, b=2} = {}) {
-	return a + b;
+function add({ a = 1, b = 2 } = {}) {
+    return a + b;
 }
 
 // 5
-add({a:3});
+add({ a: 3 });
 
-function add({a, b} = {a: 0, b: 0}) {
-	return a + b;
+function add({ a, b } = { a: 0, b: 0 }) {
+    return a + b;
 }
 
 // [3, undefined],因为已经指定了函数的默认参数
-add({a:3});
+add({ a: 3 });
 
 // 不能使用括号的情况，1 变量声明的时候 2 函数参数 3 赋值语句的模式
 
 // 下面情况不报错
 [(a)] = [3];
-({p:(b)} = {});
+({ p: (b) } = {});
 [(parseInt.prop)] = [3];
 
 // 解构赋值的作用
 
 // 交换变量的值
-var x = 3, y = 4;
+var x = 3,
+    y = 4;
 [x, y] = [y, x];
 
 // 函数返回多个值，方便取值
 function test() {
-	return [1,2];
+    return [1, 2];
 }
 
 let [a, b] = test();
 
 // 函数参数
-function f({x, y,z }) {
-	return x + y + z;
+function f({ x, y, z }) {
+    return x + y + z;
 }
 
-f({z:1, y: 2, x: 3});
+f({ z: 1, y: 2, x: 3 });
 
 // 提取json数据
 let jsonData = {
-	name : 'lifeng'
+    name: 'lifeng'
 }
 
-let {name} = jsonData;
+let { name } = jsonData;
 
 // 函数参数默认值
 
 // 遍历Map解构
 const map = new Map();
 map.set('name', 'hello');
-for(let [key, value] of map) {
-	console.log(key + ":" + value);
+for (let [key, value] of map) {
+    console.log(key + ":" + value);
 }
 
 // 表示一个字符
@@ -201,8 +201,8 @@ s.codePointAt(1);
 // 97
 s.codePointAt(2);
 
-for(let ch of s) {
-	console.log(ch.codePointAt(0).toString(16));
+for (let ch of s) {
+    console.log(ch.codePointAt(0).toString(16));
 }
 
 // 不能识别32位的UTF-16字符
@@ -210,13 +210,13 @@ String.fromCharCode(0x20BB7);
 
 String.fromCodePoint(0x20BB7);
 
-for(let code of 'fos') {
-	console.log(code);
+for (let code of 'fos') {
+    console.log(code);
 }
 
 var str = String.fromCodePoint(0x20BB7);
-for(let i = 0; i < str.length; i++) {
-	console.log(str[i]);
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
 }
 
 // normalize 用来将不同表示方法统一为同样的形式
@@ -250,10 +250,10 @@ s.endsWith('d');
 `test
 换行
 `
-let name='lifeng';
-let str= `name is ${name}`;
+let name = 'lifeng';
+let str = `name is ${name}`;
 
-let str='(name) => `${name}`';
+let str = '(name) => `${name}`';
 let fn = eval.call(null, str);
 fn('jack');
 
@@ -284,23 +284,23 @@ const array = Array.from(items);
 
 // 去除重复成员的方法
 function dedupe(array) {
-	return Array.from(new Set(array));
+    return Array.from(new Set(array));
 }
 
 for (let item of items.keys()) {
-	console.log(item);
+    console.log(item);
 }
 
-for(let item of items.values()) {
-	console.log(item);
+for (let item of items.values()) {
+    console.log(item);
 }
 
-for(let item of items.entries()) {
-	console.log(item);
+for (let item of items.entries()) {
+    console.log(item);
 }
 
-for(let item of items) {
-	console.log(item);
+for (let item of items) {
+    console.log(item);
 }
 
 items.forEach((key, value) => console.log(key + ':' + value));
@@ -309,7 +309,7 @@ let set = new Set([1, 2]);
 let arr = [...set];
 
 // {2, 4}
-set = new Set([...set].map(x=> x*2));
+set = new Set([...set].map(x => x * 2));
 
 let set = new Set([1, 2]);
 
@@ -326,20 +326,23 @@ let b = new Set([2, 3, 4]);
 // 并集 {1, 2, 3, 4}
 let union = new Set([...a, ...b]);
 // 交集 {2, 3}
-let intersect = new Set([...a].filter(x=> b.has(x)));
+let intersect = new Set([...a].filter(x => b.has(x)));
 // 差集 {1}
-let difference = new Set([...a].filter(x=> !b.has(x)));
+let difference = new Set([...a].filter(x => !b.has(x)));
 
 // 遍历产生新的数组
 set = new Set([...set].map(val => val * 2));
 
-set = new Set(Array.from(set, val=> val * 3));
+set = new Set(Array.from(set, val => val * 3));
 
-const a = [[1, 2], [3, 4]];
+const a = [
+    [1, 2],
+    [3, 4]
+];
 const ws = new WeakSet(a);
 
 const m = new Map();
-const o = {p: 'hello world'};
+const o = { p: 'hello world' };
 
 m.set(o, 'content');
 // "content"
@@ -357,14 +360,19 @@ const map = new Map([
 
 ]);
 
-const set = new Set([['foo', 2], ['bar', 2]]);
+const set = new Set([
+    ['foo', 2],
+    ['bar', 2]
+]);
 const map = new Map(set);
 // 2
 map.get('foo');
 // true
 map.has('foo');
 
-const m2 = new Map([['foo', 3]]);
+const m2 = new Map([
+    ['foo', 3]
+]);
 const m3 = new Map(m2);
 // 3
 m3.get('foo');
@@ -378,26 +386,29 @@ map.get(['a']);
 // undefine 和 null 不同的键
 // NaN虽然不是严格相等，但是认为是一样的键
 
-const map = new Map([['no', 3], ['fo', 5]]);
+const map = new Map([
+    ['no', 3],
+    ['fo', 5]
+]);
 
 // no fo
-for(let key of map.keys()) {
-	console.log(key);
+for (let key of map.keys()) {
+    console.log(key);
 }
 
 // 3 5
-for(let value of map.values()) {
-	console.log(value);
+for (let value of map.values()) {
+    console.log(value);
 }
 
 // no 3 fo 5
-for(let entry of map.entries()) {
-	console.log(entry[0], entry[1]);
+for (let entry of map.entries()) {
+    console.log(entry[0], entry[1]);
 }
 
 // no 3 fo 5
-for(let [key, value] of map) {
-	console.log(key, value);
+for (let [key, value] of map) {
+    console.log(key, value);
 }
 
 // 转换为数组
@@ -420,57 +431,57 @@ const map2 = new Map([...map].map(([key, value]) => [key, value = value * 2]));
 // key is no, value is 3
 // key is fo, value is 5
 map.forEach(function(value, key, map) {
-	console.log("key is %s, value is %d", key, value);
+    console.log("key is %s, value is %d", key, value);
 });
 
 // key is no, value is 3
 // key is fo, value is 5
 const reporter = {
-	report : function (key, value) {
-		console.log("key is %s, value is %d", key, value);
-	}
+    report: function(key, value) {
+        console.log("key is %s, value is %d", key, value);
+    }
 };
 map.forEach(function(value, key, map) {
-	this.report(key, value);
+    this.report(key, value);
 }, reporter);
 
 // Map转换为对象
 function strMapToObj(strMap) {
-	let obj = Object.create(null);
-	for(let [key, value] of strMap) {
-		obj[key] = value;
-	}
-	return obj;
+    let obj = Object.create(null);
+    for (let [key, value] of strMap) {
+        obj[key] = value;
+    }
+    return obj;
 }
 
 // 对象转换为Map
 function objToStrMap(obj) {
-	let map = new Map();
-	for(let key of Object.keys(obj)) {
-		map.set(key, obj[key]);
-	}
+    let map = new Map();
+    for (let key of Object.keys(obj)) {
+        map.set(key, obj[key]);
+    }
 
-	return map;
+    return map;
 }
 
 // Map转为Json
 function strMaptoJson(strMap) {
-	return JSON.stringify(strMapToObj(strMap));
+    return JSON.stringify(strMapToObj(strMap));
 }
 
 // 转换为数组Json
 function mapToArrayJson(map) {
-	return JSON.stringify([...map]);
+    return JSON.stringify([...map]);
 }
 
 // JSON转换为Map
 function jsonTostrMap(jsonStr) {
-	return objToStrMap(JSON.parse(jsonStr));
+    return objToStrMap(JSON.parse(jsonStr));
 }
 
 // 数组Json转换为Map
 function jsonToMap(jsonStr) {
-	return new Map(JSON.parse(jsonStr));
+    return new Map(JSON.parse(jsonStr));
 }
 
 // WeakMap只接收对象作为键名，键名是弱引用，键名指向的对象的引用对象不会增加，但是
@@ -480,21 +491,63 @@ function jsonToMap(jsonStr) {
 let myElement = document.getElementById('logo');
 let myWeakMap = new WeakMap();
 
-myWeakMap.set(myElement, {count: 0});
+myWeakMap.set(myElement, { count: 0 });
 myElement.addEventListener('click', function() {
-	let myCount = myWeakMap.get(myElement);
-	myCount.count++;
+    let myCount = myWeakMap.get(myElement);
+    myCount.count++;
 });
 
 // Count内部属性 _count 是实例的弱引用
 const _count = new WeakMap();
 class Count {
-	constructor(counter) {
-		_count.set(this, counter);
-	}
+    constructor(counter) {
+        _count.set(this, counter);
+    }
 }
 
 // 函数,默认参数每次是重新求值的
 function log(x, y = 1) {
+    console.log(x, y);
+}
+
+function foo({ x, y = 2 }) {
+    console.log(x, y)
+}
+// TypeError
+foo();
+// undefined 2
+foo({});
+// 5 3
+foo({ x: 5, y: 3 });
+
+function foo({ x, y = 2 } = {}) {
+    console.log(x, y);
+}
+// undefined 2
+foo();
+
+function foo({ x, y } = { x: 1, y: 2 }) {
+    console.log(x, y);
+}
+// 1 2
+foo();
+// 3 4
+foo({ x: 3, y: 4 });
+
+// undefined undefined
+foo({});
+
+function foo(x = 1, y) {
 	console.log(x, y);
 }
+
+// Unexpected token
+foo(, 1);
+// 1 2 if null no work
+foo(undefined, 2);
+
+// length 不计声明了默认值的参数,后面的也不计
+// 1
+(function(a, b = 2, c){console.log(a, b, c)}).length;
+
+// 一旦设置了参数的默认值，函数进行声明初始化时，会形成一个单独的作用域。
