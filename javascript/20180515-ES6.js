05794542bb75e8e4dc0cac8a3fad7e756a04d2b0
win10 <lifeng245112@126.com>
2018-06-05 22:28:09 +0800

    pipeline

--- javascript/20180515-ES6.js
+++ javascript/20180515-ES6.js
@@ -606,3 +606,17 @@ function foo() {
 		console.log(_this.id);
 	})
 }
+
+let insert = (value) => ({into: (array) => ({after: (afterValue) => {
+  array.splice(array.indexOf(afterValue) + 1, 0, value);
+  return array;
+}})});
+
+const pipeline = (...funcs) =>
+  val => funcs.reduce((a, b) => b(a), val);
+
+const plus1 = a => a + 1;
+const mult2 = a => a * 2;
+const addThenMult = pipeline(plus1, mult2);
+
+addThenMult(5);
