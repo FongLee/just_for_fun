978cb61f8e6e1268cc51e043782c844a1e1d975f
win10 <lifeng245112@126.com>
2018-06-24 11:44:09 +0800

    ES6 对象初始化

--- react_study/hackernews/src/App.js
+++ react_study/hackernews/src/App.js
@@ -15,21 +15,37 @@ const list = [
   url: 'www.google.com',
   objectId: 1
 }];
+
+const myList = {list};
+
+const userService = {
+  getUserName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list
+    };
+  }
+
   render() {
 
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
-            <div key={itme.objectId}>
-              <span>
-                  <a href={item.url}>{item.title}</a>
-              </span> 
-              <span>{item.author}</span>
-            </div>
-          )
-        })}
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+          </div>
+        )}
+        
       </div>
     );
   }
