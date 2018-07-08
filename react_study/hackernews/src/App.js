f2b094d26540b194ba8fb645ee2b345bfd522b12
win10 <lifeng245112@126.com>
2018-07-08 12:23:36 +0800

    分页抓取

--- react_study/hackernews/src/App.js
+++ react_study/hackernews/src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const list = [
   {
@@ -74,14 +76,31 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
+
+    const {hits, page} = result;
+
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+
+    const updatedHits = [
+      ...oldHits, ...hits
+    ];
+
+
     this.setState({
-      result
+      result: {
+        hits: updatedHits,
+        page
+      }
     });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`).then(response => response.json()).then(result => this.setSearchTopStories(result)).catch(e => e);
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
   }
+
 
   componentDidMount() {
     const {searchTerm} = this.state;
@@ -114,6 +133,7 @@ class App extends Component {
   }
 
   onSearchSubmit(event) {
+    debugger;
     const {searchTerm} = this.state;
 
     this.fetchSearchTopStories(searchTerm);
@@ -123,6 +143,8 @@ class App extends Component {
   render() {
 
     const {searchTerm, result} = this.state;
+
+    const page = (result && result.page) || 0;
 
     if (!result) {
       return null;
@@ -142,6 +164,11 @@ class App extends Component {
           <Table
                  list={ result.hits }
                  onDismiss={ this.onDismiss } /> }
+        <div className="interactions">
+          <Button onClick={ () => this.fetchSearchTopStories(searchTerm, page + 1) }>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
