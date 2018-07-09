3ebd9ed36fd99292d5cf41e50e6fafcc6b2aac01
win10 <lifeng245112@126.com>
2018-07-09 22:34:45 +0800

    import css node cache

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.sendfile('./html/20180709-离线存储.html');
+});
+app.get('/html/20180709-css-import.html', function(req, res) {
+	res.sendfile('./html/20180709-mainfest.manifest');
+});
+
+app.get('/html/20180709-mainfest.manifest', function(req, res) {
+	res.sendfile('./html/20180709-mainfest.manifest');
+});
+
+app.get('/html/20180709-import-test.css', function(req, res) {
+	res.sendfile('./html/20180709-import-test.css');
+});
+
+var server = app.listen(3000, function() {
+
+});
