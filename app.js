0dc27d52d47bb87aa2ffc77b3b9c138b58bb1749
win10 <lifeng245112@126.com>
2018-07-13 21:49:43 +0800

    localstorage

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.get('/html/20180709-Label.html', function(req, res) {
 	res.sendfile('./html/20180709-import-test.css');
 });
 
+app.get('/html/20180711-localStorage-test.html', function(req, res) {
+	res.sendfile('./html/20180711-localStorage-test.html');
+});
+	
+
+app.get('/html/20180713-read-localStorage.html', function(req, res) {
+	res.sendfile('./html/20180713-read-localStorage.html');
+});
+
 var server = app.listen(3000, function() {
 
 });
