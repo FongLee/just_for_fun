646b8600c1f26d42a9dd8a55288a3eebed5bdc9e
win10 <lifeng245112@126.com>
2018-08-08 21:52:35 +0800

    rand Sort

--- javascript/20180803-test.js
+++ javascript/20180803-test.js
@@ -6,3 +6,31 @@ function addPot(num) {
 	});
 
 }
+
+// 实现数组的随机排序
+function randSort(arr) {
+	for(var i = 0, len = arr.length; i < arr.length; i++) {
+		var rand = parseInt(Math.random() * len);
+		var temp = arr[rand];
+		arr[rand] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
+
+arr.sort(function() {
+	return Math.random() - 0.5;
+});
+
+function randSort(arr) {
+	var mixedArray = [];
+
+	while(arr.length > 0) {
+		var randIndex = parseInt(arr.length * Math.random());
+		mixedArray.push(arr[randIndex]);
+		arr.splice(randIndex, 1);
+
+	}
+
+	return mixedArray;
+}
