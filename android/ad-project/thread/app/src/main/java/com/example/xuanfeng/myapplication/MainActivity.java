204351967e3669d6c579700205ca8a11266ccb7b
win10 <lifeng245112@126.com>
2018-08-30 22:28:26 +0800

    aysn

--- android/ad-project/thread/app/src/main/java/com/example/xuanfeng/myapplication/MainActivity.java
+++ android/ad-project/thread/app/src/main/java/com/example/xuanfeng/myapplication/MainActivity.java
@@ -7,7 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,21 +30,62 @@ public class MainActivity extends AppCompatActivity {
                 readUrl("http://www.baidu.com");
             }
         });
+
+        tv = findViewById(R.id.tvResult);
+
     }
 
     private void readUrl(String str) {
-        new AsyncTask<String, Void, String>() {
+        new AsyncTask<String, Float, String>() {
 
 
             @Override
             protected String doInBackground(String... strings) {
 
-                URL url = new URL(arg0[0]);
+                try {
+                    URL url = new URL("http://www.baidu.com");
+                    URLConnection urlConnection =  url.openConnection();
 
-                
+                    long total = urlConnection.getContentLength();
+
+                    InputStream iStream = urlConnection.getInputStream();
+                    InputStreamReader inRead = new InputStreamReader(iStream);
+
+                    BufferedReader br = new BufferedReader(inRead);
+                    String line;
+                    StringBuffer builder = new StringBuffer();
+                    while((line = br.readLine()) !=  "") {
+                        builder.append(line);
+                        publishProgress((float)builder.toString().length() / total);
+                    }
+                    br.close();;
+                    iStream.close();
+                    return builder.toString();
+
+                } catch (Exception e) {
+
+                }
+
+
+
                 return null;
             }
+
+            @Override
+            protected void onPostExecute(String s) {
+                System.out.print(s);
+                tv.setText(s);
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected void onProgressUpdate(Float... values) {
+                System.err.print(values[0]);
+                super.onProgressUpdate(values);
+            }
         }.execute(str);
+
+
 
     }
 
